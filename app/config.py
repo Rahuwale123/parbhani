40a9eed7d020b7f2a0aaf0271d56b275e3601825
rahul wale <rahul.wale@simplifyvms.com>
2025-05-22 10:04:34 +0530

    third

--- app/config.py
+++ app/config.py
@@ -1,14 +1,8 @@
 from pydantic_settings import BaseSettings
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Settings(BaseSettings):
-    GEMINI_API_KEY: "AIzaSyAWrqDp3qZvbZlI0FM9i_4mxUIui-TtW6c"
+    GEMINI_API_KEY: str = "AIzaSyAWrqDp3qZvbZlI0FM9i_4mxUIui-TtW6c"  # Replace with your actual API key
     APP_NAME: str = "Gemini Function Calling API"
     DEBUG: bool = False
-
-    class Config:
-        env_file = None
 
 settings = Settings() 
